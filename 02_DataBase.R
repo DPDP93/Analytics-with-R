e232e9b54b16cc7613b41f8e0cf24e0e35fc609d
dprvulov <dejan.prv@gmail.com>
2020-03-20 20:53:04 +0100

    update

--- 02_DataBase.R
+++ 02_DataBase.R
@@ -3,6 +3,7 @@ library(RSQLite)
 library(dplyr)
 library(tibble)
 library(logger)
+library(lubridate)
 
 log_threshold(TRACE)
 log_layout(layout_glue_colors)
@@ -27,11 +28,18 @@ dbWriteTable(con, "jobs", jobs, overwrite = TRUE)
 dbWriteTable(con, "_jobs", dbJobs, overwrite = TRUE)
 
 
-# Log, cleanup
-print(
-    tibble(
-        tables = c("old", "existing", "new"),
-        rows = c(nrow(oldJobs), nrow(existingJobs), nrow(newJobs))
-    )
+# Old Existing New Jobs
+dbOldNew = dbReadTable(con, "oldNew")
+oldNew = tibble(
+    date = as.character(today()),
+    old = nrow(oldJobs),
+    existing = nrow(existingJobs),
+    new = nrow(newJobs)
 )
-rm(oldJobs, existingJobs, newJobs, dbJobs, jobs)
+oldNew = bind_rows(dbOldNew, oldNew)
+dbWriteTable(con, "oldNew", oldNew, overwrite = TRUE)
+dbWriteTable(con, "_oldNew", dbOldNew, overwrite = TRUE)
+
+
+print(oldNew)
+rm(oldJobs, existingJobs, newJobs, dbJobs, jobs, oldNew, dbOldNew)
